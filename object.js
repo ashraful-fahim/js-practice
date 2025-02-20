848707dd8021cb764d019a2a74cecbbb29634b26
ashraful-fahim <ashraful30june@gmail.com>
2025-02-20 23:52:24 +0600

    exploring javaScript

---
+++ object.js
@@ -0,0 +1,17 @@
+// An object can hold all types of data types.
+
+const person = {
+    name : 'Fahim',
+    age : 27,
+    hobbies : ['biking', 'gaming', 'reading books', 'learning new things'],
+    friends : {
+        zakuan : 'closest',
+        shiki : 'closest',
+        antor : 'good friend',
+        plabon : 'good friend',
+    },
+    isSingle : false,
+    movies : [{title: 'tere nam', release: 2002}, {seies: 'GOT', watched : 2017}]
+}
+
+console.log(person.movies[0].title);
