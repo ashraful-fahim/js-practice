03a562a244f7b1f7df801ac56c60a6d200014870
ashraful-fahim <ashraful30june@gmail.com>
2025-03-06 03:15:23 +0600

    added two objects and made a new object through spread operator

--- spread.js
+++ spread.js
@@ -10,3 +10,21 @@ console.log(newFriend);
 const numbers = [12,134,345,2,4,567,36,363,47];
 console.log(Math.max(numbers));
 console.log(Math.max(...numbers));
+
+
+//often used for copying and pasting and adding
+
+const circle = {
+    radius: 10
+};
+
+const style= {
+    backgroundColor: 'red'
+};
+
+const solidCircle = {
+    ...circle,
+    ...style
+};
+
+console.log(solidCircle);
