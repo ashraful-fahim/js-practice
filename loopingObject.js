7ee67c32cda42e0222a9924e2a2f218d67028cd2
ashraful-fahim <ashraful30june@gmail.com>
2025-03-06 02:42:41 +0600

    used FOR OF in objects and called it's entries

--- loopingObject.js
+++ loopingObject.js
@@ -4,6 +4,22 @@ const person = {
      sex: 'male',
 }
 
-for (let key in person){
+for (let key in person){             //for of cannot be used in looping objects
     console.log(`Key : ${key}, Value : ${person[key]}`);
 }
+
+
+/*incase of using FOR OF in objects, we have to call it's entries
+and we know the entries are returned in arrays.
+*/ 
+const cars = {
+    brand: 'Toyota',
+    model: 'corolla',
+    type: 'sedan',
+}
+
+for (let [key,value] of Object.entries(cars)){          
+     //here we destructure the returned array by assigning variables to it's index
+    console.log(`key: ${key}, value: ${value}`);
+}
+
