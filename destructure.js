1cdd4123b542dc103cb41b56a6e7e7adb72e8cc4
ashraful-fahim <ashraful30june@gmail.com>
2025-03-06 02:09:05 +0600

    learned array destructuring

--- destructure.js
+++ destructure.js
@@ -1,12 +1,21 @@
-// In the following way we destructure objects
+// Objects destructuring
 
-const person ={
+const person = {
     name: 'mokhles',
     age: 45,
     profession: 'private employee',
     isSmoker: false,
 }
 
-const {name, age, profession, isSmoker} = person;
+const { name, age, profession, isSmoker } = person;
 
-console.log(isSmoker,name);
+console.log(isSmoker, name, age);
+
+
+// Array destructuring
+
+const people = ['bappa', 'hero-alam', 'noman', {type: 'jolly'}];
+
+const [front, hero, junior, obj] = people;  //unlike object, anonymous variable names can be set according to the index of the array 
+
+console.log(front, hero, obj);
