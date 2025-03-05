6943fe38e112d675ce31bc958e1476f126faf05e
ashraful-fahim <ashraful30june@gmail.com>
2025-03-05 17:16:01 +0600

    learned more about spread operator

--- spread.js
+++ spread.js
@@ -5,3 +5,8 @@ console.log(...friends);
 
 const newFriend = ['abid', ...friends];
 console.log(newFriend);
+
+
+const numbers = [12,134,345,2,4,567,36,363,47];
+console.log(Math.max(numbers));
+console.log(Math.max(...numbers));
