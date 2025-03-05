317f2b2405029fc33bc82eb7ec13ff5aa67fb45f
ashraful-fahim <ashraful30june@gmail.com>
2025-03-06 02:23:30 +0600

    learned to access objects more deeply

---
+++ objectAdvanced.js
@@ -0,0 +1,24 @@
+const person = {
+    name: 'hena',
+    age: 23,
+    country: 'BD'
+}
+
+person.status = 'offline'; //adding a new property to the object
+
+Object.seal(person); // prevents from adding new properties to the object
+
+person.profession = 'student'; //result
+
+person.age = 24;     //replacing the value of a property
+
+Object.freeze(person); //prevents from replacing the value of properties
+
+person.age = 25; //result
+
+
+
+const keys = Object.keys(person);
+const values = Object.values(person);
+const entries = Object.entries(person);
+console.log(keys, values, entries);
