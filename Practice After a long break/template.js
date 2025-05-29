c7b4b8c5e43e1f3dd091e28b932ab4d1ebee7d8e
ashraful-fahim <ashraful30june@gmail.com>
2025-05-29 19:41:45 +0600

    practiced spread operator

--- Practice After a long break/template.js
+++ Practice After a long break/template.js
@@ -11,3 +11,10 @@ const sum = (...rest) => {
 
 const result = sum(1,2,3);
 console.log(result);
+
+
+const numbers =  [1,2,3,4,5,6,7];
+// console.log(...numbers)
+
+const newNumbers = [...numbers, 8, 9, 10];
+console.log(newNumbers);
