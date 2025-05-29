949b02b230cb40355bf2ad3fd36544110483e865
ashraful-fahim <ashraful30june@gmail.com>
2025-05-29 18:26:17 +0600

    practiced arrow function

--- Practice After a long break/template.js
+++ Practice After a long break/template.js
@@ -2,3 +2,12 @@ const fname = "Ashraful";
 const lname = "lname";
 
 console.log(`i am ${lname}`);
+
+
+
+const sum = (...rest) => {
+    return rest;
+}
+
+const result = sum(1,2,3);
+console.log(result);
